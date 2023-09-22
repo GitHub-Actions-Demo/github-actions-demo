3527ef4fbc88d8b53799982119876c7f4da2a77b
Nicholas Fantozz <nicholas.fantozz@skechers.com>
2023-09-22 15:01:36 -0400

    Testing artifact action

---
+++ .github/actions/get-artifact-name/index.js
@@ -0,0 +1,19 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+
+try {
+  const prNumber = core.getInput('pr-number');
+  const artifactNameInput = core.getInput('artifact-name');
+  const defaultName = 'app-main';
+
+  console.log('prNumber', prNumber);
+  console.log('artifactName', artifactNameInput);
+  // If we have a PR number use it or the default value
+  let artifactName = prNumber ? `app-pr-${prNumber}` : defaultName;
+  // If we have an incoming name use it or the previous value
+  artifactName = artifactNameInput ? artifactNameInput : artifactName;
+
+  core.setOutput("artifact-name", artifactName);
+} catch (error) {
+  core.setFailed(error.message);
+}
